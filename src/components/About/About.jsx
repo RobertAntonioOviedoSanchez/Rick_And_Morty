86827d79411fe2255b1dfa5b592576ae5743fc58
RobertAntonioOviedoSanchez <robertoviedo35@gmail.com>
2023-04-13 16:57:38 -0400

    actualizado

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,9 @@ import Style from "./About.module.css"
 const About = () => {
     return(
         <div>
-            <h1>Soy el About</h1>
+            <h1>Acerca de mi</h1>
+            <p>     Mi nombre es Robert Oviedo, tengo 19 años y soy de Venezuela. Actualmente estoy cursando el Bootcamp de SoyHenry para poder adquirir todos los conocimientos que me ofrece la cursada y lograr conseguir un certificado y empleo como Full Stack Web Developer.</p>
+            <p>     Este proyecto sobre Rick and Morty ha sido creado durante la instancia de Bootcamp. Mediante pasaba el tiempo y las clases, por medio de homeworks y horas de estudio logré crear este proyecto.</p>
         </div>
     )
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,8 @@
-import { ADD_FAV, REMOVE_FAV } from "./action-types";
+import { ADD_FAV, FILTER, ORDER, REMOVE_FAV } from "./action-types";
 
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharactersFav: [], //es como una copia de respaldo de myFavorites
 }
 
 
@@ -10,16 +11,37 @@ const reducer = (state = initialState, action) => {
         case ADD_FAV:
             return {
                 ...state,
-                myFavorites: [ ...state.myFavorites, action.payload ]
+                myFavorites: [ ...state.allCharactersFav, action.payload ],
+                allCharactersFav: [ ...state.allCharactersFav, action.payload]
             }
+
         case REMOVE_FAV:
             return {
                 ...state,
                 myFavorites: state.myFavorites.filter(fav => fav.id !== action.payload)
             }
 
-            
-             
+        case FILTER:
+            const allCharactersFiltered = state.allCharactersFav.filter(character => character.gender === action.payload)
+            return {
+                ...state,
+                myFavorites: 
+                action.payload === "allCharacters" 
+                ? [...state.allCharactersFav]
+                : allCharactersFiltered
+                
+            }
+
+        case ORDER:
+            const allCharactersFavCopy = [...state.allCharactersFav]
+            return {
+                ...state,
+                myFavorites: action.payload === "A" ? allCharactersFavCopy.sort((a, b) => a.id - b.id) : allCharactersFavCopy.sort((a,b) => b.id - a.id)  //sort() para ordenar como en M1
+                
+            }
+        
+
+          
         default: 
             return {...state}
             
